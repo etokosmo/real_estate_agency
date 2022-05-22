a96eabed46551b6c00a7d2ecd402639b49c4976d
Denis Polyakov <p-ov.da@ya.ru>
2022-05-22 15:56:20 +0300

    Add inline to flats

--- property/admin.py
+++ property/admin.py
@@ -3,14 +3,20 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ("owner",)
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ["town", "address",]
+    search_fields = ["town", "address", ]
     readonly_fields = ["created_at"]
     list_display = (
         "address", "price", "new_building", "construction_year", "town",)
     list_editable = ("new_building",)
     list_filter = ("new_building", "rooms_number", "has_balcony")
     raw_id_fields = ("liked_by",)
+    inlines = [FlatInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):

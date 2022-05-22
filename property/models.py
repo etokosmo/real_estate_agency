91942316e0064cb8ff4c4d133e8ae563da3aadff
Denis Polyakov <p-ov.da@ya.ru>
2022-05-22 15:38:23 +0300

    Remove owner from Flat.model to Owner.model

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, null=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -67,10 +64,10 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, null=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    flats = models.ManyToManyField(Flat, related_name="owners_flats", verbose_name='Квартиры владельца')
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, null=True, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    flats = models.ManyToManyField(Flat, related_name="owners_flats", verbose_name='Квартиры владельца', db_index=True)
 
     def __str__(self):
         return f'{self.owner}'
